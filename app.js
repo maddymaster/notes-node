32fd2e8e852c736859eaa215a3ffe76a7f605d15
madhusudhan <maddy@getambee.com>
2018-08-30 14:38:14 +0530

    First commit after adding nodemon

---
+++ app.js
@@ -0,0 +1,54 @@
+console.log('Starting app.js');
+
+const fs = require('fs');
+const os = require('os');
+const _ = require('lodash');
+const notes = require('./notes');
+
+//console.log(_.isString(true));
+//console.log(_.isString('Verint'));
+
+//console.log('Result: ', notes.add(9, -3));
+
+var filteredArray = _.uniq([1,2,3,3,3,1,1,2,2,'Google',1, 2, 3, 'Google']);
+console.log(filteredArray);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//var user = os.userInfo();
+
+//var res = notes.addNote();
+//console.log(res);
+//console.log(user);
+
+//fs.appendFile('greetings.txt', `Hello  ${user.username}! You are ${notes.age} years old!`,  (err) => {
+    //console.log('The "data to append" was appended to file!');
+  //});
+//console.log(notes);
